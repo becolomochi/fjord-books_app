b44b22e026320e78934bc6784bd979ce87a978ca
becolomochi <becolomochi@gmail.com>
2021-07-18 16:06:52 +0900

    トップにアクセスしたらusersにリダイレクトさせる

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+
+  get '/', to: redirect('/users')
+
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations',
