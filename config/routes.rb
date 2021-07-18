24a22d12d2fb50420339f703b5c94f4b79f5d5b1
becolomochi <becolomochi@gmail.com>
2021-07-18 14:52:26 +0900

    ユーザー一覧と詳細ページを作成

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
     get 'reset_password', to: 'users/passwords#edit'
   end
 
+  resources :users
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
