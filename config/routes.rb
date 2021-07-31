2c1e2ca192d9b90200535d1efa8130d43c21deeb
becolomochi <becolomochi@gmail.com>
2021-07-31 16:48:19 +0900

    routesの整理

--- config/routes.rb
+++ config/routes.rb
@@ -3,17 +3,7 @@ Rails.application.routes.draw do
 
   get '/', to: redirect('/users')
 
-  devise_for :users, controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations',
-  }
-  devise_scope :user do
-    get 'sign_in', to: 'users/sessions#new'
-    get 'sign_up', to: 'users/registrations#new'
-    get 'forgot_password', to: 'users/passwords#new'
-    get 'reset_password', to: 'users/passwords#edit'
-  end
-
+  devise_for :users
   resources :books, :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
