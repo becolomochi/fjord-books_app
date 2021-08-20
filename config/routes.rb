2968e9885848bf31940165437a70faa51501a637
becolomochi <becolomochi@gmail.com>
2021-08-20 16:02:14 +0900

    フォローフォロワー一覧の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'books#index'
+  get '/users/:id/following', to: 'users#following'
+  get '/users/:id/followers', to: 'users#followers'
   resources :books
   resources :users, only: %i(index show)
   resources :relationships, only: %i(create destroy)
