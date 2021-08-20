84dd0019a1e6b2314b0a8e0a5e7e156d7ea03f36
becolomochi <becolomochi@gmail.com>
2021-08-20 14:44:45 +0900

    Userモデルにfollowingの関連付けを追加・メソッドを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,20 @@ class User < ApplicationRecord
   has_one_attached :avatar
 
   has_many :active_relationships, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+
+  # ユーザーをフォロー
+  def follow(other_user)
+    following << other_user
+  end
+
+  # ユーザーをフォロー解除
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # 現在のユーザーがフォローしているかどうかの判定
+  def following?(other_user)
+    following.include?(other_user)
+  end
 end
