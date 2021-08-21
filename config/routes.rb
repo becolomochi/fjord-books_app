6798a73f467edd0ae15f94c6c4273cc18da6898b
becolomochi <becolomochi@gmail.com>
2021-08-21 15:48:13 +0900

    フォロー一覧フォロワー一覧のルーティングをuserの中に入れる

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'books#index'
-  get '/users/:id/followings', to: 'users#followings'
-  get '/users/:id/followers', to: 'users#followers'
   resources :books
-  resources :users, only: %i(index show)
+  resources :users, only: %i(index show) do
+    member do
+      get 'followings'
+      get 'followers'
+    end
+  end
   resources :relationships, only: %i(create destroy)
 end
