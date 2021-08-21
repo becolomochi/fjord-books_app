38824411a14cf23d7c1deae1f7dcca5eb1f5f024
becolomochi <becolomochi@gmail.com>
2021-08-21 11:17:34 +0900

    フォローを単数形から複数形に修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,12 @@ class User < ApplicationRecord
 
   has_many :active_relationships, inverse_of: 'follower', class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy
   has_many :passive_relationships, inverse_of: 'followed', class_name: 'Relationship', foreign_key: 'followed_id', dependent: :destroy
-  has_many :following, through: :active_relationships, source: :followed
+  has_many :followings, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
 
   # ユーザーをフォロー
   def follow(other_user)
-    following << other_user
+    followings << other_user
   end
 
   # ユーザーをフォロー解除
@@ -23,6 +23,6 @@ class User < ApplicationRecord
 
   # 現在のユーザーがフォローしているかどうかの判定
   def following?(other_user)
-    following.include?(other_user)
+    followings.include?(other_user)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'books#index'
-  get '/users/:id/following', to: 'users#following'
+  get '/users/:id/followings', to: 'users#followings'
   get '/users/:id/followers', to: 'users#followers'
   resources :books
   resources :users, only: %i(index show)
