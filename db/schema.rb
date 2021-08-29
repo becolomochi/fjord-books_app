0d500ef032837d4452085db3afb78beee4b763e1
becolomochi <becolomochi@gmail.com>
2021-08-29 11:06:57 +0900

    dbのテーブルを作り直す
    - テーブル名をfollow_relationshipsに
    - カラムをnull:falseに

--- db/schema.rb
+++ db/schema.rb
@@ -49,14 +49,14 @@ ActiveRecord::Schema.define(version: 2021_08_20_051144) do
     t.string "picture"
   end
 
-  create_table "relationships", force: :cascade do |t|
-    t.integer "follower_id"
-    t.integer "followed_id"
+  create_table "follow_relationships", force: :cascade do |t|
+    t.integer "follower_id", null: false
+    t.integer "followed_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["followed_id"], name: "index_relationships_on_followed_id"
-    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
-    t.index ["follower_id"], name: "index_relationships_on_follower_id"
+    t.index ["followed_id"], name: "index_follow_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_follow_relationships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_follow_relationships_on_follower_id"
   end
 
   create_table "users", force: :cascade do |t|
