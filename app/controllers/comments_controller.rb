72606830c23bb4adef870e6dba789c425b37fa09
becolomochi <becolomochi@gmail.com>
2021-09-11 12:06:29 +0900

    rubocop対応。before_actionの記述を移動

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class CommentsController < ApplicationController
+  before_action :set_commentable, only: %i[create]
+
   def create
     @comment = @commentable.comments.build(comment_params)
 
